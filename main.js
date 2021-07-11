cce0b2974ef0101b8f031eb4531118143c78fc1e
Yuriy Titov <yuranus@gmail.com>
2021-07-11 23:57:06 +0300

    add lesson material and code challendge

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             image: '/assets/images/socks_green.jpg',
             inventory: 10,
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {id: 2234, color: 'green', image: '/assets/images/socks_green.jpg'},
