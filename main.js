8cee749e317b6b776365b164c136b5c1405b6306
Yuriy Titov <yuranus@gmail.com>
2021-07-12 00:50:52 +0300

    add lesson material and code challendge

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: '/assets/images/socks_green.jpg',
-            inventory: 10,
-            inStock: false,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            // image: '/assets/images/socks_green.jpg',
+            // inventory: 10,
+            // inStock: false,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: '/assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: '/assets/images/socks_blue.jpg'},
+                {id: 2234, color: 'green', image: '/assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: '/assets/images/socks_blue.jpg', quantity: 0},
             ],
             sizes: ['s', 'm', 'l'],
         }
@@ -21,8 +24,27 @@ const app = Vue.createApp({
         removeFromCart() {
             this.cart -= 1
         },
-        updateImage(img) {
-            this.image = img
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index)
         },
+        
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+            return this.brand + ' ' + this.product
+        }
+        return ''
+    }
     },
 })
