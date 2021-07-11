b40ef37c02371db70a79eed85c646070a5a5f45f
Yuriy Titov <yuranus@gmail.com>
2021-07-11 23:42:40 +0300

    add lesson material and code challendge

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: '/assets/images/socks_green.jpg',
             inventory: 10,
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                {id: 2234, color: 'green', image: '/assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: '/assets/images/socks_blue.jpg'},
             ],
             sizes: ['s', 'm', 'l'],
         }
-    }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(img) {
+            this.image = img
+        },
+    },
 })
