3aebd12a4b308a72072873f4e1de3244548148ea
Yuriy Titov <yuranus@gmail.com>
2021-07-11 22:43:25 +0300

    add lesson matirial and code challendge

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: '/assets/images/socks_green.jpg',
-            url: './assets/images/socks_green.jpg'
+            inventory: 10,
+            onSale: true,
         }
     }
 })
